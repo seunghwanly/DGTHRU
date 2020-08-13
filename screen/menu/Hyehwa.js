bfe15a7f0383802e35fcd9e7a71ddf65cb1c92ad
Seunghwanly <seunghwanly@gmail.com>
2020-08-13 21:37:23 +0900

    메뉴 가져오기

--- screen/menu/Hyehwa.js
+++ screen/menu/Hyehwa.js
@@ -1,30 +1,81 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, SectionList } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 import { enableScreens } from 'react-native-screens';
 
-import HyehwaDessert from './data/HyehwaDessert.json';
-
+//json
+import * as data from './data/HyehwaDessert.json';
 
 enableScreens();
+
+const drinkData = data.categories_drink;
+
+const CategroyInfo = ({ category, menu }) => {
+
+  const MenuDetail = () => {
+    const mapToList = (data) => {
+      return data.map((menu, i) => {
+        return (
+          <MenuInfo
+            menu={menu}
+            key={i}
+          />
+        );
+      });
+    };
+    return (
+      <View style={{flexDirection:'row', margin:10}}>
+        { mapToList(category.menu)}
+      </View>
+    )
+  }
+
+  const MenuInfo = ({ menu }) => {
+    return (
+      <View>
+        <Text>{menu.name}</Text>
+        <Text>{menu.cost}</Text>
+      </View>
+    )
+  }
+
+  return (
+    <View>
+      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{category.category_name}</Text>
+      <ScrollView
+        horizontal='true'
+        >
+        <MenuDetail />
+      </ScrollView>
+    </View>
+  );
+}
+
+const CategoryMenu = () => {
+  const mapToComponent = (data) => {
+    return data.map((category, i) => {
+      return (
+        <CategroyInfo
+          category={category}
+          key={i}
+        />);
+    });
+  };
+  return (
+    <View>
+      { mapToComponent(drinkData)}
+    </View>
+  );
+}
 
 function Hyehwa({ navigation }) {
 
   return (
-    <View style={{ flex: 1, flexDirection: 'column' }}>
-      <Text >
-        HyehwaDessert List
-           </Text>
-      <FlatList
-        data={HyehwaDessert}
-        showsVerticalScrollIndicator={false}
-        renderItem={({ item }) =>
-          <View >
-            <Text>{item.name}</Text>
-          </View>
-        }
-        keyExtractor={(item, index) => index.toString()}
-      />
+    <View style={{ flex: 1, flexDirection: 'column', padding:10 }}>
+      <ScrollView>
+        <CategoryMenu />
+      </ScrollView>
     </View>
   );
 }
