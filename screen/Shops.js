16c53e74fd8ffdd5dfbd3ca9ec5725bbddefb2df
Seunghwanly <seunghwanly@gmail.com>
2020-08-13 15:33:02 +0900

    shops.js 생성

--- screen/Shops.js
+++ screen/Shops.js
@@ -3,7 +3,9 @@ import {
     View,
     Text,
     StyleSheet,
-    Button
+    Button,
+    FlatList,
+    TouchableOpacity
 } from 'react-native';
 
 import auth from '@react-native-firebase/auth';
@@ -14,7 +16,74 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 enableScreens();
 
-function Shops({navigation}) {
+const shopData = [
+    {
+        id: 'hyehwa_1f',
+        title: '혜화관 1층 카페',
+        location: '혜화관 1층'
+    },
+    {
+        id: 'munhwa_1f',
+        title: '문화관 지하1층 카페',
+        location: '문화관 지하1층'
+    },
+    {
+        id: 'main_outdoor',
+        title: '본관 야외 카페',
+        location: '본관 야외 휴게장소'
+    },
+    {
+        id: 'singong_1f',
+        title: '신공학관 1층 카페',
+        location: '신공학관 1층'
+    }
+];
+
+const Item = ({ title, location, navigation }) => (
+    <TouchableOpacity
+        style={{ width: 300 }}
+        onPress={() => alert('title' + { title } + 'location' + {location})}
+    >
+        <View style={{ flexDirection: 'row', margin: 5, padding: 5, alignItems:'center' }}>
+            <View style={
+                {
+                    borderRadius: 25,
+                    width: 25,
+                    height: 25,
+                    backgroundColor: 'deepskyblue'
+                }
+            }
+            />
+            <View style={
+                {
+                    backgroundColor: 'lightgray',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flex: 1,
+                    marginStart: 20,
+                    borderRadius: 12,
+                    padding:5
+                }
+            }>
+                <Text style={
+                    {
+                        fontSize: 20,
+                        fontWeight: 'bold',
+                    }
+                }>{title}</Text>
+            </View>
+        </View>
+        <View>
+            <Text style={{ textAlign: 'right', color: 'gray', fontSize: 12, marginEnd:10 }}>{location}</Text>
+        </View>
+    </TouchableOpacity>
+);
+
+function Shops({ navigation }) {
+
+    const renderItem = ({ item }) => (
+        <Item title={item.title} location={item.location} />
+    );
 
     signOut = () => {
         auth()
@@ -22,14 +91,30 @@ function Shops({navigation}) {
             .then(() => console.log('User Signed Out !'));
     }
 
-    return(
+    return (
         <>
             <View style={styles.background}>
-                <Text style={styles.text}>Welcome !</Text>
-                <Button 
-                    title='로그아웃'
-                    onPress={() => signOut()}
-                />
+                <View style={styles.header}>
+                    <Text style={styles.title}>DGTRHU</Text>
+                    <Text style={styles.subtitle}>동국대학교 CAFE LIST</Text>
+                </View>
+
+                <View style={styles.body}>
+                    <FlatList
+                        data={shopData}
+                        renderItem={renderItem}
+                        keyExtractor={item => item.id}
+                    />
+                </View>
+                <View style={styles.footer}>
+                    <Button
+                        title='로그아웃'
+                        onPress={() => signOut()}
+                    />
+                </View>
+
+
+
             </View>
         </>
     );
@@ -41,12 +126,34 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
-        backgroundColor: 'white'
+        backgroundColor: 'white',
+        padding: '10%',
+        flex: 1
     },
-    text : {
-        fontSize:44,
-        fontWeight:'bold',
-        textAlign:'center'
-    }
+    header: {
+        height: '20%',
+        width: '100%'
+    },
+    body: {
+        height: '60%',
+        width: '100%'
+    },
+    footer: {
+        height: '20%',
+        width: '100%',
+    },
+    title: {
+        fontSize: 44,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    subtitle: {
+        fontSize: 22,
+        color: 'gray',
+        textAlign: 'center'
+    },
+    item: {
+
+    },
 });
 export default Shops;
