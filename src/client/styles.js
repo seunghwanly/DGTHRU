4be18a17875422b9a5e2291946eef8543dcd1787
jswboseok <62234148+jswboseok@users.noreply.github.com>
2020-09-05 18:42:53 +0900

    앗 실수

--- src/client/styles.js
+++ src/client/styles.js
@@ -2,29 +2,31 @@ import { StyleSheet } from 'react-native';
 
 const clientStyles = StyleSheet.create({
     background: {
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center',
+        // width: '100%',
+        // height: '100%',
+        justifyContent:'center',
         alignItems: 'center',
-        flexDirection: 'column',
         backgroundColor: 'white',
-        padding: '3%',
-        //margin: '5%',
-        flex: 1
+        padding: 10,
+        flex: 6
     },
     header: {
         flex:1,
-        //height: '20%',
-        width: '100%',
-        //margin: 5,
-        alignItems: 'center',
+        // width: '100%',
+        marginTop:20,
+        
     },
     body: {
-        flex: 6,
-        width: '100%',
-        //height: '90%',
-        alignItems: 'center',
-        margin: 3,
+        flex:4,
+        // width: '100%',
+        margin:5,
+        
+    },
+    footer: {
+        flex:1,
+        // width: '100%',
+        justifyContent:'center'
+        
     },
     title: {
         fontSize: 40,
@@ -32,27 +34,65 @@ const clientStyles = StyleSheet.create({
         textAlign: 'center'
     },
     subTitle: {
-        fontSize: 15,
+        fontSize: 22,
         color: 'gray',
         textAlign: 'center'
     },
+    components: {
+        fontSize: 20,
+    },
+    phoneNumber: {
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 8,
+        backgroundColor: 'white',
+        paddingBottom: 10,
+        paddingTop: 10,
+        paddingStart: 40,
+        paddingEnd: 40,
+        margin: 10,
+        fontSize: 15,
+        width: 240,
+        textAlign: 'center'
+
+    },
+    appleButton: {
+        width: 200,
+        height: 45,
+        margin: 'auto',
+        alignSelf: 'center'
+    },
     itemWrapper: {
         flexDirection: 'row',
-        margin: 4,
-        padding: 4,
-        flex: 1,
+        margin: 5,
+        padding: 5,
         alignItems: 'center'
+    },
+    itemCircle: {
+        borderRadius: 25,
+        width: 25,
+        height: 25,
+        backgroundColor: 'cornflowerblue'
+    },
+    itemNameBar: {
+        backgroundColor: 'ghostwhite',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        marginStart: 20,
+        borderRadius: 12,
+        padding: 5
     },
     itemDesc: {
         fontSize: 15,
-        color: 'black',
         fontWeight: 'bold',
         textAlign: 'center'
     },
     itemSubDesc: {
-        textAlign: 'center',
+        textAlign: 'right',
         color: 'gray',
         fontSize: 10,
+        marginEnd: 10
     },
     image: {
         flex: 1,
@@ -73,6 +113,7 @@ const clientStyles = StyleSheet.create({
         alignItems: 'center',
         margin: 10, 
     },
+    
 
 });
 
