e059ac410b9000db68aa0ac7632713d0ee6abb0c
jswboseok <62234148+jswboseok@users.noreply.github.com>
2020-09-20 23:29:47 +0900

    change

--- src/supervisor/example.js
+++ src/supervisor/example.js
@@ -7,163 +7,68 @@ import database from '@react-native-firebase/database';
 import auth from '@react-native-firebase/auth';
 import moment from 'moment';
 
+export default class sales extends Component{
 
-var currentTime = moment().format('YYYY_MM_DD');
-var shopname = '';
-var phonenumber = '';
-async function DeleteOrderList(key) {
-    var userPath = 'hyehwa_roof/' + currentTime + '/+821012341234/' + key + '/';
-
-    console.log(userPath);
-
-    await database()
-        .ref(userPath)
-        .remove();
-}
-
-async function Setconfirm(shopname, date, phonenum, key){
-    database().ref('shops/' + shopname + '/' + date + '/' + phonenum + '/' + key).update({orderState: 'confirm'});
-}
-
-async function SetUnconfirm(shopname, date, phonenum, key){
-    database().ref('shops/' + shopname + '/' + date + '/' + phonenum + '/' + key).update({orderState: 'request'});
-}
-
-async function SetReady(shopname, date, phonenum, key){
-    database().ref('shops/' + shopname + '/' + date + '/' + phonenum + '/' + key).update({orderState: 'ready'});
-}
-
-export default class Example extends Component {
-
-    constructor(props){
+    constructor(props) {
         super(props);
-        shopname = "hyehwa_roof";
-        this.state={ 
-            list:[],
-        } }
-
+        this.state = {
+            index: 0,
+            totalCost: 0,
+            list: [],
+            shopname: "hyehwa_roof",
+        }
+    }
+    
     componentDidMount(){
+        //console.log('key: ' + shopname);
         
-        database().ref('shops/' + shopname).on('value', (snapshot) =>{
+
+        database().ref('admin/' + this.state.shopname).on('value', (snapshot) => {
             var li = []
+            var index = 0;
+            var tempTotalCost = 0;
+            //snapshot: 날짜
+
             snapshot.forEach((childSnapShot) => {
-            var orderDate = childSnapShot.key
-            childSnapShot.forEach((child)=>{
-                var phonenumber = child.key
-                child.forEach((menuChild)=>{
-                    console.log('orderTime2 : ' , menuChild.val().orderTime)
-                    li.push({
-                        key: menuChild.key,
-                        phonenum: phonenumber,
-                        date: orderDate,
-                        name: menuChild.val().name,
-                        orderTime: menuChild.val().orderTime,
-                        cost: menuChild.val().cost,
-                        count: menuChild.val().count,
-                        cup : menuChild.val().cup,
-                        shotnum : menuChild.val().shotnum,
+                //ChildSnapshot : 주문 날짜
+                var orderDate = childSnapShot.key;
+                    childSnapShot.forEach((menuChild) => {
+                        var keyName = menuChild.key;
+                        this.state.totalCost += menuChild.val().cost;
+                        console.log("cc >>> dd ?? " + menuChild.val().cost);
+                        li.push({
+                            //key : index++,
+                            listSize: 1,
+                            isGroup: false,
+                            cost: menuChild.val().cost,
+                            name: menuChild.val().name,
+                            options: menuChild.val().options,
+                            orderInfo: menuChild.val().orderInfo,
+                            //key : orderInfo.orderTime,
+                            key: keyName,
+                            date:orderDate
+                        })
                     })
-                })
-            })
-            
             })
             const Moment = require('moment')
-            console.log('what? : ' , li);
-            li.sort((d2, d1) => new Moment(d2.orderTime,'HH:mm:ss') - new Moment(d1.orderTime,'HH:mm:ss'));
-            console.log('after? : ' , li);
-            this.setState({list:li})
-           // this.sortListByTime(li)
-        })
-     
-    }
-
-    //deleteItem(k) {
-    //    let forward = this.state.list.slice(0,k)
-    //    //console.log(`for : `,forward);
-      
         
+            console.log("temptotalcost >>>>    " + this.state.totalCost);
 
-    //    for(var i = k + 1;k<this.state.list.length;i++){
-    //        this.state.list[i].index = this.state.list[i].index - 1;
-    //    }
+            li.sort((d2, d1) => new Moment(d2.orderInfo.orderTime, 'HH:mm:ss') - new Moment(d1.orderInfo.orderTime, 'HH:mm:ss'));
 
-    //    let back = this.state.list.slice(k+1,this.state.list.length)
-    //    //console.log(`back : `,back);
-      
-    //    let total = forward.concat(back)	//forward¿¡ back ºÙÀÌ±ë
-    //    //console.log(`total : `,total);
-    //    this.setState({
-    //        list:total
-    //    })
-    //}
+            this.setState({ list: li });
 
-    //deleteorderlist(k){
-    //    var arr = this.state.list;
-    //    this.setState({
-    //        list: arr.filter(arr => arr.key !== k)
-    //    })
-
-    //    await database()
-    //    .ref('hyehwa_roof/2020_08_22/+821012341234/' + k)
-    //    .remove();
-    //}
-
-    
-
-    // componentDidMount() {
-    //   //const currentUser = auth().currentUser.uid;
-    //   database().ref('hyehwa_roof/2020_08_21')
-    //     .on('value', snapshot => {
-    //       const lists = _.map(snapshot.val(), (uid) => {
-    //         return {uid}
-    //       });
-    //       this.setState({lists, loading: false})
-    //     })
-    // } 
-    //  renderItem({item}) {
-    //   return (
-    //       <ListItem item={item} />
-    //     )
-    // }
-
-    sortListByTime(){
-        this.state.list.sort(function(obj1, obj2) {
-           // return obj1.cost - obj2.cost;
-         //return new moment(obj1.orderTime). -new Date(obj2.orderTime).getTime().valueOf;
-         var date1 = new Date(obj1.orderTime);
-         var date2 = new Date(obj2.orderTime);
-         return date2 - date1;
-       });
-       this.setState(previousState => (
-         { list: previousState.list }
-       ))
-     }
-  
-    render() {
-        return (
-            <View style={exampleStyle.background}>
-                <Text>[{shopname}] : Order List</Text>
-                    <FlatList style={{width:'100%'}}
-                    data={this.state.list}
-                    keyExtractor={item => item.key}
-                    renderItem={({item})=>{
-                    return(
-                        <View  style={exampleStyle.listbox}>    
-                            <Text style={exampleStyle.orderlisttext}>{item.name}  {item.cup} {item.count}</Text>
-                            <Text style={exampleStyle.orderlisttext}> {item.orderTime}</Text>
-                            <View style={exampleStyle.orderlistview}>
-                                <Button 
-                                style={exampleStyle.buttonstyle}
-                                title="승인취소" onPress={() => SetUnconfirm(shopname, item.date , item.phonenum, item.key)}></Button> 
-                                <Button  style={{margin:5}}
-                                title="주문승인" onPress={() => Setconfirm(shopname, item.date , item.phonenum, item.key)}></Button> 
-                                <Button  style={{margin:5}}
-                                title="준비완료" onPress={() => SetReady(shopname, item.date , item.phonenum, item.key)}></Button> 
-                            </View>
-                        </View>)
-                    }}
-                />
-            </View>
-        );
+        })
+        for(var i = 0;i<this.state.list.length;i++){
+            console.log("cccc >>  " + this.state.list[i].cost)
+        }
+    }
+    render(){
+       
+        return(
+            <Text>
+                helloworld
+            </Text>
+        )
     }
 }
