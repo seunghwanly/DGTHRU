570664b385396c61b7f20cb89bd89814c9afa3fe
JungIn-Choi <wd32777@gmail.com>
2020-11-09 22:08:37 +0100

    coupon UI

--- src/client/drawer/Coupon.js
+++ src/client/drawer/Coupon.js
@@ -5,19 +5,22 @@ import {
     Text,
     FlatList,
     View,
-    Modal, Button
+    Modal, 
+    StyleSheet
 } from 'react-native';
 import ReceiptSingleModal from '../../utils/ReceiptSingleModal';
 import ReceiptGroupModal from '../../utils/ReceiptGroupModal';
 import { Header } from 'react-native-elements';
-import { BillStyles, CircleStyles } from './styles';
+import { BillStyles, CircleStyles, couponStyles } from './styles';
 import auth from '@react-native-firebase/auth';
 import moment from 'moment';
+import { clientStyles } from '../styles';
 import database from '@react-native-firebase/database'
 
 //firebase
 import { userCouponTotalDatabase } from '../../utils/DatabaseRef';
 import { firebase } from '@react-native-firebase/database';
+import { color } from 'react-native-reanimated';
 
 export default class Coupon extends React.Component {
 
@@ -178,31 +181,28 @@ export default class Coupon extends React.Component {
                         paddingRight: 15
                     }}>
                     <View style={{ backgroundColor: 'white', flex: 1, padding: 10, borderTopStartRadius: 12, borderTopEndRadius: 12 }}>
-                        <View style={{ flexDirection: 'column', marginBottom: 5, borderBottomWidth: 1, padding: 8 }}>
+                        <View style={{ flexDirection: 'column', marginBottom: 5, padding: 8 }}>
                             <Text style={{ fontWeight: 'bold', width: '100%' }}>현재 쿠폰 {this.state.couponNum} 장</Text>
                         </View>
                         <View >
-                            <View style={{ flexDirection: 'row', marginBottom: 5, padding: 8 }}>
-                            </View>
-                            <View style={{ marginBottom: 5, padding: 8 }}>
-                                <View style={{ flexDirection: 'row', marginBottom: 5, padding: 8 }}>
+                            <View style = {couponStyles.imageContainer}>
+                                <View style={{ flexDirection: 'row', marginBottom: 5, padding: 8}}>
                                     {this.couponImage(0)}
                                     {this.couponImage(1)}
                                     {this.couponImage(2)}
                                     {this.couponImage(3)}
                                     {this.couponImage(4)}
                                 </View>
-                                <View style={{ flexDirection: 'row', marginBottom: 5, padding: 8, }}>
+                                <View style={{ flexDirection: 'row', marginBottom: 5, padding: 8 }}>
                                     {this.couponImage(5)}
                                     {this.couponImage(6)}
                                     {this.couponImage(7)}
                                     {this.couponImage(8)}
                                     {this.couponImage(9)}
                                 </View>
-                                <Text style={{ textAlign: "right" }}>10장 아메리카노 1잔</Text>
-
+                                <Text style={{ textAlign: 'right' }}>10장 아메리카노 1잔</Text>
                             </View>
-                            <View>
+                            <View style = {couponStyles.imageContainer}>
                                 <View style={{ flexDirection: 'row', marginBottom: 5, padding: 8 }}>
                                     {this.couponImage(10)}
                                     {this.couponImage(11)}
@@ -211,29 +211,6 @@ export default class Coupon extends React.Component {
                                     {this.couponImage(14)}
                                 </View>
                                 <Text style={{ textAlign: "right" }}>15장 카페라떼 1잔</Text>
-
-                                {/* <FlatList
-                                    contentContainerStyle={{ margin: 4 }}
-                                    horizontal={false}
-                                    numColumns={5}
-                                    data={this.state.shopInfo}
-                                    renderItem={
-                                        ({ item }) => (
-                                            <View>
-                                                <ImageLinker
-                                                    name={item}
-                                                    style={{
-                                                        width: 68,
-                                                        height: 68,
-                                                        marginVertical: 2,
-                                                        marginTop: 15
-                                                    }} />
-                                            </View>
-                                        )
-                                    }
-                                    keyExtractor={item => item.key}
-                                >
-                                </FlatList> */}
                             </View>
                         </View>
                     </View>
